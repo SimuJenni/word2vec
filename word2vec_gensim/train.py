85042907cdb0664e7b7f2a7069598597247f0a24
Simon Brulhart <simon@brulhart.me>
2016-04-10 23:50:07 +0200

    Improve unified training script

--- word2vec_gensim/train.py
+++ word2vec_gensim/train.py
@@ -1,4 +1,8 @@
+#!/usr/bin/env python3
+
 import logging
+import os
+import pickle
 import sys
 from pathlib import Path
 
@@ -8,20 +12,17 @@ from gensim.models.word2vec import Text8Corpus
 
 data_dir = Path('../word2vec_data')
 corpus = Text8Corpus('../word2vec_data/text8')
-# import ipdb; ipdb.set_trace()
-
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-
-
-# Train models with different learning rates
-alphas = list(4**x for x in range(-4, 6))
-
-# for a in alphas:
-#     model = gensim.models.Word2Vec(corpus, alpha=a, workers=16)
-#     model.save('../word2vec_data/results/alpha_'+str(a))
+parameters = [
+    ('alpha', [4**x for x in range(-4, 6)]),     # learning rate
+    ('window', list(range(2, 12))),              # window size
+    ('size', list(range(25, 301, 25)))           # vector size
+]
 
 
 def setup():
+    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',
+                        level=logging.INFO)
+
     if not data_dir.exists():
         logging.fatal('No "../word2vec_data" folder!')
         logging.fatal('Please create it or run the script from the right directory.')
@@ -30,28 +31,39 @@ def setup():
         (data_dir / folder).mkdir(exist_ok=True)
 
 
-def run_trainings(corpus, param, values):
+def run_trainings():
+    for param, values in parameters:
+        for value in values:
+            train(corpus, param, value)
+
+
+def train(corpus, param, value):
+    """Compute, save and return word2vec model with `param` set to `value.`
+
+    Only load the pre-computed model if it already exists on disk.
+    """
+    default_params = dict(sentences=corpus, iter=10, workers=os.cpu_count())
+    # Note: cpu_count() also counts "logical" cores (Hyper-threading))
+
     corpus_name = Path(corpus.fname).name
-    default_params = dict(sentences=corpus, workers=16, iter=10)
+    fname = '{}_{}_{}'.format(corpus_name, param, value)
+    dest_path = data_dir / 'results' / fname
+    if dest_path.exists():
+        logging.info("'{}' already exists, don't train again".format(fname))
+        with dest_path.open('rb') as f:
+            return pickle.load(f)
 
-    for val in values:
-        fname = '{}_{}_{}'.format(corpus_name, param, val)
-        dest_path = data_dir / 'results' / fname
-        if dest_path.exists():
-            logging.info("'{}' already exists, don't train again".format(fname))
-            continue
-
-        logging.info("Start training '{}'".format(fname))
-        params = default_params.copy()
-        params[param] = val
-        model = gensim.models.Word2Vec(**params)
-        model.save(dest_path.as_posix())
-
-
+    logging.info("Start training '{}'".format(fname))
+    params = default_params.copy()
+    params[param] = value
+    model = gensim.models.Word2Vec(**params)
+    model.save(dest_path.as_posix())
+    return model
 
 
 def main():
     setup()
+    run_trainings()
 
 
 if __name__ == '__main__':
