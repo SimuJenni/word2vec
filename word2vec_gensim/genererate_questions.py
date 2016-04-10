2b7b07f57a31b09852ad887d8dc1e26c0456bb2d
Simon Jenni <simujenni@students.unibe.ch>
2016-04-10 14:24:43 +0200

    Added code for generating new evaluation data
    Added questions for brands and their country of origin

---
+++ word2vec_gensim/genererate_questions.py
@@ -0,0 +1,16 @@
+from __future__ import print_function
+import csv
+
+source = 'Brand_Data.csv'
+target = 'brands_questions.txt'
+
+with open(source, 'rU') as f:
+    reader = csv.reader(f)
+    rows = [row for row in reader]
+
+with open(target, 'w+') as outfile:
+    print(": Brand-Origin", file=outfile)
+    for row in rows:
+        for otherRow in rows:
+            if not row[0] == otherRow[0]:
+                print(row[0]+' '+row[1]+' '+otherRow[0]+' '+otherRow[1], file=outfile)
