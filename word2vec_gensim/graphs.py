68d3a81693768fc0403ea632d2dbbd69a3dc2c0a
Simon Brulhart <simon@brulhart.me>
2016-04-17 12:26:43 +0200

    Finish graphs.py script
    Known issues:
    * Weird result for multiple values of skipgram
    * This kind of graph doesn't make sense for hs=0/1

--- word2vec_gensim/graphs.py
+++ word2vec_gensim/graphs.py
@@ -1,51 +1,91 @@
 #!/usr/bin/env python3
 import pickle
+import sys
 from collections import namedtuple
+from itertools import groupby
 from pathlib import Path
 
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as pyplot
 
 
-source_path = Path('../word2vec_data/eval_cbow')
+source_path = Path('../word2vec_data/')
+pretty = {
+    'window': 'window size',
+    'size': 'vector size',
+    'alpha': 'learning rate (alpha)',
+    'hs': 'hierarchical softmax'
+}
 
 
-Result = namedtuple('Result', ['corpus', 'parameter', 'value', 'skipgram', 'fpath'])
+class Result(namedtuple('Result', ['corpus', 'parameter', 'value', 'skipgram', 'fpath'])):
+    """Represent the resulting file of an accuracy evaluation"""
+
+    def load(self):
+        with self.fpath.open('rb') as f:
+            return pickle.load(f)
+
+    @classmethod
+    def parse_filename(cls, fpath):
+        fpath = Path(fpath)
+        corpus, parameter, value = fpath.name.split('_')
+        skipgram = fpath.resolve().match('eval_sg/*')
+        return cls(corpus, parameter, value, skipgram, fpath)
 
 
-def parse_filename(fpath):
-    fpath = Path(fpath)
-    corpus, parameter, value = fpath.name.split('_')
-    skipgram = False  # TODO
-    return Result(corpus, parameter, value, skipgram, fpath)
+def plot_parameter_graph(parameter):
+    """Plot the accuracy of word2vec models for different values of 'parameter'
+
+    Plot for each corpus, both for skipgram and cbow"""
+    fig = pyplot.figure()
+    plt = fig.add_subplot(111)
+
+    for (corpus, skipgram), cases in find_parameter_cases(parameter):
+        plot_values = []
+        for result in cases:
+            data = result.load()
+            accuracies = [
+                len(sect['correct']) / (len(sect['correct']) + len(sect['incorrect']))
+                for sect in data if len(sect['correct']) + len(sect['incorrect']) > 0
+            ]
+            average = sum(accuracies) / len(accuracies)
+            plot_values.append((float(result.value), average))
+
+        label = "corpus: {}, skipgram: {}".format(corpus, skipgram)
+        xs, ys = zip(*sorted(plot_values))
+        plt.plot(xs, ys, label=label)
+
+    plt.set_xlabel(pretty[parameter].capitalize())
+    plt.set_ylabel('Accuracy')
+    plt.legend(loc='best')
+    fig.suptitle("Accuracy for different values of {}".format(pretty[parameter]))
+
+    graph_path = source_path / 'graphs'
+    graph_path.mkdir(exist_ok=True)
+    graph_path /= 'graph_acc_{}.pdf'.format(parameter)
+    fig.savefig(graph_path.as_posix())
 
 
-def plot_parameter_graph(parameter, results):
-    fig = plt.figure()
-    # ax1 = fig.add_axes([1, 1, 1, 1])
-    ax1 = fig.add_subplot(111)
+def find_parameter_cases(parameter):
+    """Return results grouped by their value of skipgram and corpus.
 
-    plot_values = []
-    for fpath in source_path.iterdir():
-        if not fpath.match('*_%s_*' % parameter):
-            continue
-        result = parse_filename(fpath)
-        with fpath.open('rb') as f:
-            data = pickle.load(f)
-        accuracy = sum(
-            len(sect['correct']) / (len(sect['correct']) + len(sect['incorrect']))
-            for sect in data
-        )
-        plot_values.append((int(result.value), accuracy))
+    Output example: [
+        (('Text8', False), [<results>]),
+        (('Text8', True), [<results>]),
+        ...
+    ]"""
+    pattern = 'eval_*/*_%s_*' % parameter
+    results = [Result.parse_filename(f) for f in source_path.glob(pattern)]
+    results.sort()
+    return groupby(results, lambda r: (r.corpus, r.skipgram))
 
-    label = "corpus: {}".format("Text8")  # TODO
 
-    # ax1.plot(plot_values, label=label)
-    xs, ys = zip(*sorted(plot_values))
-    ax1.plot(xs, ys, label=label)
-    # ax1.set_xticks(xticks)
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: graphs.py <parameter>")
+        sys.exit(1)
 
-    ax1.legend(loc='best')
-    fig.suptitle("Accuracy")
-    fig.savefig('abcd.png')
+    plot_parameter_graph(sys.argv[1])
 
-plot_parameter_graph('window', 33)
+
+if __name__ == '__main__':
+    main()
